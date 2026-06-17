955e39abce929ab00f6c1c4296eae02565abaeb9
Nikola258 <nikolic07nikola@gmail.com>
2026-06-17 13:19:51 +0200

    Toegevoegd: - de post kan nu een image uploaden - de image wordt opgeslagen in de database - als je de pagina refreshed dan blijft de image nog op de page Hoe heb ik dit opgelost? - er was geen image prop en img tag dit heb ik dus toegevoegd

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,10 +1,10 @@
-function Posts({ user, content, date, onDelete }) {
+function Posts({ user, content, date, image, onDelete }) {
   return (
     <div>
       <p>{content}</p>
+      {image && <img src={image} alt="post" style={{ maxWidth: '100%' }} />}
       <small>{user}</small>
       <small>{date}</small>
-
       {onDelete && <button onClick={onDelete}>Delete</button>}
     </div>
   );
